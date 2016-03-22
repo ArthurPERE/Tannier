8310b938eb4a0b85f56b3f5d2a5d2d977d17224c
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-03-22 13:59:30 +0100

    modification du fichier longueur

--- longueur.py
+++ longueur.py
@@ -1,15 +1,24 @@
-fichier = raw_input('nom du fichier > ')
+fichier1 = "1348_European_Black_Death_main_chromosome_2012.fasta"
+fichier2 = "Yersinia_pestis_CO92.fasta"
 
-f = open(fichier, 'r')
+f1 = open(fichier1, 'r')
+f2 = open(fichier2, 'r')
 
-a = f.readlines()
-a.pop(0)
-a.pop(-1)
-
-longueur = 0
-
-for i in a:
-	longueur += len(i[:-1])
+def lon(f):
+	a = f.readlines()
+	a.pop(0)
+	a.pop(-1)
 	
+	longueur = 0
+	
+	for i in a:
+		longueur += len(i[:-1])
+		
+	return(longueur)
+		
+		
+l1, l2 = lon(f1), lon(f2)
+print 'longueur du chromosome au moyen age : ', l1
+print 'longueur du chromosome mainetenant : ', l2
 
-print longueur
+print 'difference de longueur entre avant et maintenant : ', l1 - l2
